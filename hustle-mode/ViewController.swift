5ee40ded4f6e58000c4fa8d4a46b4ed684b690ec
Burning <ynwangpeng@gmail.com>
2018-11-13 16:47:57 +0800

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Burning on 2018/11/13.
+//  Copyright © 2018 Burning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
